99cbc52537665be92ed05de68e27a9dcf5bc1554
Aditya Shankar <aditya.shankar24@imperial.ac.uk>
2025-07-16 04:19:13 +0400

    v0
    Test of ISP Calcs

---
+++ Code/ISP_Calculations.py
@@ -0,0 +1,6 @@
+from rocketcea.cea_obj import CEA_Obj
+import numpy as np
+ispObj = CEA_Obj( oxName='N2O', fuelName='ETHANOL')
+s = ispObj.get_full_cea_output( Pc=30, MR=np.arange(4.6, 4.7, 0.02), eps=4.59, short_output=1, pc_units='bar')
+
+print( s )
